36f22356606e0203adfdcb36edcd4e9511e6596e
CJavatX <cdpm98@hotmail.com>
2025-02-18 15:31:53 -0600

    Implementación del verifyToken

--- src/controllers/whatsapp.controller.js
+++ src/controllers/whatsapp.controller.js
@@ -1,5 +1,17 @@
 const verifyToken = (req, res) => {
-  res.send("hola verifytoken");
+  try {
+    const accessToken = "ASHCSTOSasdasdasddwASDASDAS";
+    const token = req.query("hub.verify_token");
+    const challenge = req.query("hub.challenge");
+
+    if (challenge !== null && token !== null && token == accessToken) {
+      res.send(challenge);
+    } else {
+      res.status(400).send();
+    }
+  } catch (error) {
+    res.status(400).send();
+  }
 };
 
 const reciveMessage = (req, res) => {
